90e0b11ee6dc3de48f0946f3933e804d8e0e97a7
abhis <abhis@ABHISHEK>
2024-12-25 18:52:31 +0530

    Add new files in Domain model directory

---
+++ BloggingMVC/Models/Domain/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace BloggingMVC.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public String DisplayName { get; set; }
+
+        //Used to define a one-to-many or many-to-many relationship between entities
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
