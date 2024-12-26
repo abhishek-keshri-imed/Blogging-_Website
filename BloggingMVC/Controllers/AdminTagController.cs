f85b32343fd53a61c11e4caec311b2645ed2e250
abhis <abhis@ABHISHEK>
2024-12-26 14:56:39 +0530

    Added AdminTagController & addTagpage for admin with Razor

---
+++ BloggingMVC/Controllers/AdminTagController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingMVC.Controllers
+{
+    
+    public class AdminTagController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
